2098346f2fa4919be0d30a473c5657c991f70262
DavidNgumbau <99338450+DavidNgumbau@users.noreply.github.com>
2022-03-14 14:54:30 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-/*
-main - prints phrase "Programming is like building a multilingual puzzle" , followed by new line.
-*/
+/* main - prints phrase "Programming is like building a multilingual puzzle" , followed by new line. */
 
 int main(void)
 {
