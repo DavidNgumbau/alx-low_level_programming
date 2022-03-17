f3532ad30672c94eaf985a7eaa7db7913bd3788c
DavidNgumbau <99338450+DavidNgumbau@users.noreply.github.com>
2022-03-17 22:04:02 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
 * mul - multiplies two integers
-* @a - int to be multiplied by b
-* @b - int to be multiplied by a
+* int a - int to be multiplied by b
+* int b - int to be multiplied by a
 * Return: result of the operation
 */
 int mul(int a, int b)
