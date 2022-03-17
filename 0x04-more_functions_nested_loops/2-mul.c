bde67827aa85db020835208e23e06c73744f11f7
DavidNgumbau <99338450+DavidNgumbau@users.noreply.github.com>
2022-03-17 23:44:49 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
-#include "main.h'
+#include "holberton.h"
 
 /**
-* mul - multiplies two integers
-* int a - int to be multiplied to b
-* int b - int to be multiplied to a
-* Return: the result of the operation
-*/
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
 int mul(int a, int b)
 {
 	int c;
